20a16a4b4fd73aa16fa7174f897c1b11f137efee
MattWill23 <127MWilliams@frhsd.com>
2026-06-08 12:50:55 -0400

    fdsf

---
+++ script.js
@@ -0,0 +1,67 @@
+const aircraft = {
+    "F-22 Raptor": {
+        img: "images/f22.jpg",
+        role: "Air Superiority Stealth Fighter",
+        stats: {
+            speed: 95,
+            stealth: 100,
+            attack: 90,
+            range: 75
+        }
+    },
+
+    "F-35 Lightning II": {
+        img: "images/f35.jpg",
+        role: "Multirole Stealth Fighter",
+        stats: {
+            speed: 85,
+            stealth: 95,
+            attack: 85,
+            range: 80
+        }
+    },
+
+    "A-10 Thunderbolt II": {
+        img: "images/a10.jpg",
+        role: "Close Air Support Aircraft",
+        stats: {
+            speed: 60,
+            stealth: 30,
+            attack: 100,
+            range: 70
+        }
+    },
+
+    "F-15E Strike Eagle": {
+        img: "images/f15e.jpg",
+        role: "Long Range Strike Fighter",
+        stats: {
+            speed: 90,
+            stealth: 60,
+            attack: 95,
+            range: 90
+        }
+    },
+
+    "Su-57 Felon": {
+        img: "images/su57.jpg",
+        role: "5th Gen Russian Stealth Fighter",
+        stats: {
+            speed: 92,
+            stealth: 85,
+            attack: 90,
+            range: 80
+        }
+    },
+
+    "B-2 Spirit": {
+        img: "images/b2.jpg",
+        role: "Stealth Strategic Bomber",
+        stats: {
+            speed: 65,
+            stealth: 100,
+            attack: 100,
+            range: 100
+        }
+    }
+};
