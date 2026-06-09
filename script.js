b79de544d9f45b43e42375cd081dd9dbdc43b113
MattWill23 <127MWilliams@frhsd.com>
2026-06-09 14:01:44 -0400

    sswsw

--- script.js
+++ script.js
@@ -1,67 +1,40 @@
-const aircraft = {
-    "F-22 Raptor": {
-        img: "images/f22.jpg",
-        role: "Air Superiority Stealth Fighter",
-        stats: {
-            speed: 95,
-            stealth: 100,
-            attack: 90,
-            range: 75
-        }
+const cars = {
+    "Tesla Model S": {
+        make: "Tesla",
+        model: "Model S",
+        year: 2023,
+        category: "Sports Car",
+        color: "Red",
+        price: "$89,990",
+        photo: "images/model-s.jpg",
+        fuelType: "Electric",
+        horsepower: 1020,
+        seatingCapacity: 5
     },
 
-    "F-35 Lightning II": {
-        img: "images/f35.jpg",
-        role: "Multirole Stealth Fighter",
-        stats: {
-            speed: 85,
-            stealth: 95,
-            attack: 85,
-            range: 80
-        }
+    "Ford F-150": {
+        make: "Ford",
+        model: "F-150",
+        year: 2022,
+        category: "Truck",
+        color: "Black",
+        price: "$55,000",
+        photo: "images/f150.jpg",
+        fuelType: "Gasoline",
+        horsepower: 400,
+        seatingCapacity: 6
     },
 
-    "A-10 Thunderbolt II": {
-        img: "images/a10.jpg",
-        role: "Close Air Support Aircraft",
-        stats: {
-            speed: 60,
-            stealth: 30,
-            attack: 100,
-            range: 70
-        }
-    },
-
-    "F-15E Strike Eagle": {
-        img: "images/f15e.jpg",
-        role: "Long Range Strike Fighter",
-        stats: {
-            speed: 90,
-            stealth: 60,
-            attack: 95,
-            range: 90
-        }
-    },
-
-    "Su-57 Felon": {
-        img: "images/su57.jpg",
-        role: "5th Gen Russian Stealth Fighter",
-        stats: {
-            speed: 92,
-            stealth: 85,
-            attack: 90,
-            range: 80
-        }
-    },
-
-    "B-2 Spirit": {
-        img: "images/b2.jpg",
-        role: "Stealth Strategic Bomber",
-        stats: {
-            speed: 65,
-            stealth: 100,
-            attack: 100,
-            range: 100
-        }
+    "Toyota RAV4": {
+        make: "Toyota",
+        model: "RAV4",
+        year: 2023,
+        category: "SUV",
+        color: "Blue",
+        price: "$32,000",
+        photo: "images/rav4.jpg",
+        fuelType: "Hybrid",
+        horsepower: 219,
+        seatingCapacity: 5
     }
 };
